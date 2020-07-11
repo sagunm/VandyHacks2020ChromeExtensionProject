5eaf76115d6163267a65eb27f34d84bb3e32b3cd
Elena Chen <yue.chen@student.allenisd.org>
2020-07-11 12:20:12 -0500

    added alarm.js with alarm functions and updated others accordingly

--- background.js
+++ background.js
@@ -1,1 +1,12 @@
 console.log("background works");
+
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('index.html',{
+     id: 'main',
+     bounds:{width:620, heightL:500}
+    });
+});
+
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+    console.log("Got an alarm! hi from baby yoda", alarm);
+  });
